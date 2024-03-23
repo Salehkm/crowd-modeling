480af66e7062d3bce2bff4c6a2543347a42ac893
Salehkm <40856846+Salehkm@users.noreply.github.com>
2024-03-24 00:16:38 +0300

    Update crowd_model.py

--- crowd_model.py
+++ crowd_model.py
@@ -127,74 +127,8 @@ p2=Pedestrians(
                f_drive=np.array([0,0]),
                f_ab=np.array([0,0]))
 
-# Updating function, to be repeatedly called by the animation
-
 first=True
-# create animation with 10ms interval, which is repeated,
 ani = FuncAnimation(fig, update, blit=True, repeat=True,
                     frames=200)
 
 plt.show()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# # Function to update the position of each person
-# def update(frame, scat, data):
-#     data[:, 0:2] += data[:, 2:4]  # Update positions
-#     crowd= np.array([[3], 
-#         [4]] )
-#     scat.set_offsets(crowd)  # Set new positions for the scatter plot
-    
-#     return scat,
-
-
-
-
-
-# # Create initial plot
-# fig, ax = plt.subplots()
-# ax.set_xlim(-5, 5)
-# ax.set_ylim(-5, 5)
-# scat = ax.scatter([], [])  # Scatter plot for people
-
-
-# # Initial positions and velecoties 
-# # P1(X=-4,Y=0,Vx=0.09,Vy=0) P2(X=4,Y=0,Vx=-0.09,Vy=0)
-# crowd= np.column_stack([[-4,4], [0,0], [0.09,-0.09],[0.09, 0]])
-# print(crowd[:, 2:4])
-# # 
-# # Update function for animation
-# ani = FuncAnimation(fig, update, frames=200, fargs=(scat, crowd), blit=True)
-
-
-# plt.show()
-
-
-# #will be deleted
-
-# # # Function to generate initial position and speed of each person
-# # def gen_data():
-# #     x = np.random.uniform(-5, 5, N)  # Random initial x positions
-# #     y = np.random.uniform(-5, 5, N)  # Random initial y positions
-# #     vx = np.random.uniform(-0.1, 0.1, N)  # Random initial x velocities
-# #     vy = np.random.uniform(-0.1, 0.1, N)  # Random initial y velocities
-# #     return np.column_stack([x, y, vx, vy])
-# # Generate initial data
-# # data = gen_data()
